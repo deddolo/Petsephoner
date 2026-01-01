56f5c0436dba9daddfe0bacd773cdeea1bc92d92
Davide Giannuzzi <deddolo@gmail.com>
2025-12-31 19:49:57 -0600

    Add project files.

---
+++ Petsephoner/Petsephoner/Feeder.cs
@@ -0,0 +1,28 @@
+﻿
+using System.Device.Gpio;
+using System.Device.Gpio.Drivers;
+
+namespace Petsephoner
+{
+    public class Feeder
+    {
+        public static Feeder Instance = new Feeder();
+        
+        private GpioController _controller;
+        private const int ledPin = 18;
+
+        public Feeder()
+        {
+            _controller = new GpioController(new RaspberryPi3Driver());
+
+            _controller.OpenPin(ledPin, PinMode.Output);
+
+            _controller.Write(ledPin, PinValue.Low);
+        }
+
+        public async Task SetLed(bool value)
+        {
+            _controller.Write(ledPin, value ? PinValue.High : PinValue.Low);
+        }
+    }
+}
